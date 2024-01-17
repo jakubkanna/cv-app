aa04abb405bee253d5635d5baf8a041a86bfa5a3
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-01-17 12:26:20 +0100

    init

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+const data = {
+  general: {
+    name: "Andy",
+    s_name: "Warhol",
+    email: "andywarhy@yahoo.com",
+    p_nb: "000 123 999",
+  },
+  education: {
+    bachelor: {
+      school: "Your School Name",
+      study: "Your Study Title",
+      startYear: "2015",
+      endYear: "2018",
+    },
+  },
+  experience: {
+    work: {
+      company: "Your Company Name",
+      position: "Your Position Title",
+      responsibilities: "Main Responsibilities of Your Job",
+      startDate: "dd/mm/year",
+      endDate: "dd/mm/year",
+    },
+  },
+};
+
+export { data };
